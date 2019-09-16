1b1e8942cb5690329e592a3d098036429a8760f2
Shelby Frazier <fraziers@grinnell.edu>
2019-09-16 00:20:23 -0500

    Rails server and rails test work

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,12 @@ module Cookbook
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    if File.read('/proc/1/cgroup').include?('docker')
+      # https://stackoverflow.com/a/24716645/4862360
+      host_ip = `/sbin/ip route|awk '/default/ { print $3 }'`.strip
+
+      BetterErrors::Middleware.allow_ip!(host_ip) if defined?(BetterErrors::Middleware)
+      config.web_console.whitelisted_ips << host_ip
+    end
   end
 end
