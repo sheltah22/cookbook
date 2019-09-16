c0ce048317752e46e524e7e884e9575a5749537d
Shelby Frazier <fraziers@grinnell.edu>
2019-09-16 00:46:30 -0500

    Docker works again

--- config/application.rb
+++ config/application.rb
@@ -14,13 +14,6 @@ module Cookbook
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-    if File.read('/proc/1/cgroup').include?('docker')
-      # https://stackoverflow.com/a/24716645/4862360
-      host_ip = `/sbin/ip route|awk '/default/ { print $3 }'`.strip
-
-      BetterErrors::Middleware.allow_ip!(host_ip) if defined?(BetterErrors::Middleware)
-      config.web_console.whitelisted_ips << host_ip
-    end
+    # the framework and any gems in your application. 
   end
 end
