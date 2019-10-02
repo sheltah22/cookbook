30b96acb691525be11ece401b390baddd77c096e
Shelby Frazier <fraziers@grinnell.edu>
2019-10-01 20:05:31 -0500

    Add title testing

--- Gemfile
+++ Gemfile
@@ -51,6 +51,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'spring-commands-rspec'
 end
 
 group :test do
@@ -64,6 +65,7 @@ group :test do
   gem 'minitest-reporters', '~> 1.1'
   gem 'guard', '~> 2.13'
   gem 'guard-minitest', '~> 2.4'
+  gem 'rspec-rails'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/requests/page_requests_spec.rb
+++ spec/requests/page_requests_spec.rb
@@ -2,33 +2,39 @@ require 'spec_helper'
 require 'rails_helper'
 
 describe "Cookbook" do
-  it "loads root and renders home view" do
+  let(:base_title) { "Cookbook" }
+  it "loads root and renders home view with correct title" do
     get root_url
     expect(response).to have_http_status(:ok)
     expect(response).to render_template(:home)
+    assert_select "title", base_title
   end
 
-  it "loads home and renders view" do
+  it "loads home and renders view with correct title" do
     get home_url
     expect(response).to have_http_status(:ok)
     expect(response).to render_template(:home)
+    assert_select "title", base_title
   end
 
-  it "loads advanced_search and renders view" do
+  it "loads advanced_search and renders view with correct title" do
     get advanced_search_url
     expect(response).to have_http_status(:ok)
     expect(response).to render_template(:advanced_search)
+    assert_select "title", "Advanced Search | #{base_title}"
   end
 
-  it "loads add_recipe and renders view" do
+  it "loads add_recipe and renders view with correct title" do
     get add_recipe_url
     expect(response).to have_http_status(:ok)
     expect(response).to render_template(:add_recipe)
+    assert_select "title", "Add Recipe | #{base_title}"
   end
 
-  it "loads saved_recipes and renders view" do
+  it "loads saved_recipes and renders view with correct title" do
     get saved_recipes_url
     expect(response).to have_http_status(:ok)
     expect(response).to render_template(:saved_recipes)
+    assert_select "title", "Saved Recipes | #{base_title}"
   end
 end
