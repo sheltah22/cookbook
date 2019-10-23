9ded02a329716f5b6c8b866d2af7c6abf79927af
Shelby Frazier <fraziers@grinnell.edu>
2019-10-22 21:04:20 -0500

    Refactor displaying ingredients

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -4,4 +4,16 @@ class Ingredient < ApplicationRecord
   belongs_to :recipe
 
   validates :amount, presence: true
+
+  def as_string
+    @food = self.food
+    @amount = self.amount
+    @measurement = self.measurement
+    if (@amount == 1)
+      @count = 1
+    else
+      @count = 2
+    end
+    "#{@amount.truncate(2)} #{measurement.name.pluralize(@count)} #{food.name}"
+  end
 end
