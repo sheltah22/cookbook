25fc6efc7d193694efac16fb5ce4fa894bde91f5
Shelby Frazier <fraziers@grinnell.edu>
2019-10-26 15:58:57 -0500

    Flesh out show users page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   get '/add_recipe', to: 'cookbook#add_recipe'
   get '/advanced_search', to: 'cookbook#advanced_search'
   get '/saved_recipes', to: 'cookbook#saved_recipes'
+
+
   root 'cookbook#home'
+
+  resources :users
 end
