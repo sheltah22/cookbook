32c56b415825ec04358cb23cf0f824f6da884294
Shelby Frazier <fraziers@grinnell.edu>
2019-10-29 12:20:48 -0500

    Add ingredient forms with ajax requests

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,10 @@
 module RecipesHelper
+  def link_to_add_ingredient(name, f, ingredients)
+    new_ingredient = f.object.send(ingredients).build
+    id = new_ingredient.object_id
+    fields = f.fields_for(ingredients, new_ingredient, child_index: id) do |ff|
+      render('add_ingredient', f: ff)
+    end
+    link_to(name, '#', class: "add_ingredient", data: {id: id, fields: fields.gsub("\n", "")})
+  end
 end
