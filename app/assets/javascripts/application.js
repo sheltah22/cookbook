357416db1c363e36dfec9dbd4b2a8e6b55f41e00
Shelby Frazier <fraziers@grinnell.edu>
2019-10-30 17:26:53 -0500

    Take out unused code from previous jquery strategy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,5 @@
 //= require turbolinks
 //= require jquery
 //= require jquery_ujs
+//= require nested_form_fields
 //= require_tree .
--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,10 +1,2 @@
 module RecipesHelper
-  def link_to_add_ingredient(name, f, ingredients)
-    new_ingredient = f.object.send(ingredients).build
-    id = new_ingredient.object_id
-    fields = f.fields_for(ingredients, new_ingredient, child_index: id) do |ff|
-      render('ingredient_form', f: ff)
-    end
-    link_to(name, '#', class: "add_ingredient", data: {id: id, fields: fields.gsub("\n", "")})
-  end
 end
