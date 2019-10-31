9590313c28a6746d73b9f65dfb452586880093ea
Shelby Frazier <fraziers@grinnell.edu>
2019-10-30 20:42:11 -0500

    Make some tests pass

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -4,6 +4,14 @@ module Helpers
   end
 
   def logged_in_user
-    User.find(session[:user_id])
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+
+
+  def log_in_as(user, password: 'longpassword')
+    post login_path, params: { session: { email: user.email,
+                                          password: password }}
   end
 end
